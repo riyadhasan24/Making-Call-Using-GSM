a405a6c1f99f4827ddbc414025b8b0a2eb4361f7
Md. Riyad Hasan <66812569+riyadhasan24@users.noreply.github.com>
2023-06-14 18:27:18 +0600

    Add files via upload

---
+++ Making_Call_Using_GSM/Making_Call_Using_GSM.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+SoftwareSerial MY_GSM(8, 9);
+
+char Phone_No[] = "+8801314607526";
+#define Button  A1 // This Button for call command
+
+void setup() 
+{ 
+  Serial.begin(9600);
+  MY_GSM.begin(9600);
+  pinMode(Button, INPUT_PULLUP);
+}
+
+void loop() 
+{
+  if (digitalRead (Button) == 0)
+    {
+      Make_Call(Phone_No);
+    }
+  delay(5);
+}
+
+
+void Make_Call(char *number) 
+{
+  MY_GSM.print("ATD + "); 
+  MY_GSM.print(number); 
+  MY_GSM.println(";");
+  delay(15000);       // wait for 20 seconds...
+  MY_GSM.println("ATH"); //hang up
+  delay(100);
+}
